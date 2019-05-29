cbba105028bc7aa35df426d1c9fa65c191000f54
dustinpfister <dustin.pfister@gmail.com>
2019-05-29 10:43:02 -0400

    clean gitlog.js

--- demos/gitlog.js
+++ demos/gitlog.js
@@ -1,12 +1,14 @@
 let spawn = require('child_process').spawn;
 
 // start get log process
-let git = spawn('git', ['log','--format=%s']);
+let git = spawn('git', ['log', '--format=%s']);
 
 // process data
+buf = Buffer.alloc(0);
 git.stdout.on('data', (data) => {
-    let raw = data.toString();
-    console.log(raw);
+    //let raw = data.toString();
+    //console.log(data);
+    buf = Buffer.concat([buf, data])
 });
 
 // if process error
@@ -16,5 +18,13 @@ git.stderr.on('data', (data) => {
 
 // when process is done
 git.on('close', (code) => {
-    console.log('process done with code: ' + code);
+    //console.log('process done with code: ' + code);
+    let subjects = buf.toString().split('\n');
+    subjects.pop(); // pop the last empty string element
+
+    // log all subject names
+    subjects.forEach((sub) => {
+        console.log(sub);
+    });
+
 });
