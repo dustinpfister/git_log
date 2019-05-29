af9d88d93fa5241963b33d38770eb7deff0e5b48
dustinpfister <dustin.pfister@gmail.com>
2019-05-29 10:58:53 -0400

    clean up

--- demos/log_subjects_filter.js
+++ demos/log_subjects_filter.js
@@ -1,26 +1,17 @@
-let spawn = require('child_process').spawn;
-
-// start get log process
-let git = spawn('git', ['log', '--format=%s']);
-
-// process data
+let spawn = require('child_process').spawn,
+git = spawn('git', ['log', '--format=%s']),
 buf = Buffer.alloc(0);
 git.stdout.on('data', (data) => {
-    //let raw = data.toString();
-    //console.log(data);
     buf = Buffer.concat([buf, data])
 });
-
-// if process error
 git.stderr.on('data', (data) => {
     console.log(data.toString());
 });
 
 // when process is done
 git.on('close', (code) => {
-    //console.log('process done with code: ' + code);
     let subjects = buf.toString().split('\n');
-    subjects.pop(); // pop the last empty string element
+    subjects.pop();
 
     // filter by post_update:id-[id number]:v-[version number]
     subjects = subjects.filter((sub) => {
